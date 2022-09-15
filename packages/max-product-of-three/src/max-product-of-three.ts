9582c6bbca22ec30ee9f6a864f5edd3e7ce7972a
Joao Franco <joao.franco@jsfsi.com>
2022-09-15 22:13:02 +0100

    Added exercise

--- packages/max-product-of-three/src/max-product-of-three.ts
+++ packages/max-product-of-three/src/max-product-of-three.ts
@@ -1,7 +1,26 @@
 export const maximalProductOfTriplet = (list: number[]) => {
-  const maximalTriplet = list[0] + list[1] + list[2];
+  list.sort((a, b) => a - b);
 
-  for (let i = 3; i < list.length; i += 1) {}
-
-  return maximalTriplet;
+  return Math.max(
+    list[list.length - 1] * list[list.length - 2] * list[list.length - 3],
+    list[list.length - 1] * list[0] * list[1],
+  );
 };
+
+// export const maximalProductOfTriplet = (list: number[]) => {
+//   let maxProduct = list[0] * list[1] * list[2];
+
+//   for (let i = 0; i < list.length - 2; i += 1) {
+//     for (let z = i + 1; z < list.length - 1; z += 1) {
+//       for (let y = z + 1; y < list.length; y += 1) {
+//         const product = list[i] * list[z] * list[y];
+
+//         if (product > maxProduct) {
+//           maxProduct = product;
+//         }
+//       }
+//     }
+//   }
+
+//   return maxProduct;
+// };
--- packages/max-product-of-three/test/max-product-of-three.test.ts
+++ packages/max-product-of-three/test/max-product-of-three.test.ts
@@ -1,10 +1,19 @@
 import { maximalProductOfTriplet } from '../src/max-product-of-three';
 
 it.each`
-  input                   | output
-  ${[-3, 1, 2, -2, 5, 6]} | ${60}
-`(' value of the maximal product of any triplet', ({ input, output }) => {
-  const maxProduct = maximalProductOfTriplet(input);
+  timeout | input                         | output
+  ${5}    | ${[-3, 1, 2, -2, 5, 6]}       | ${60}
+  ${5}    | ${[-10, -2, -4]}              | ${-80}
+  ${5}    | ${[-5, 5, -5, 1, 1, 1, 1, 4]} | ${125}
+  ${168}  | ${new Array(1000).fill(1)}    | ${1}
+`(
+  ' value of the maximal product of any triplet $input.length $output',
+  ({ timeout, input, output }) => {
+    const startDate = +new Date();
+    const maxProduct = maximalProductOfTriplet(input);
 
-  expect(maxProduct).toEqual(output);
-});
+    const endDate = +new Date();
+    expect(maxProduct).toEqual(output);
+    expect(endDate - startDate).toBeLessThan(timeout);
+  },
+);
