99c01b9ed08758793cb2e8fb0d281c80587dd801
Joao Franco <joao.franco@jsfsi.com>
2024-09-25 18:51:16 +0100

    Improve tests

--- packages/01-brackets/test/index.test.ts
+++ packages/01-brackets/test/index.test.ts
@@ -1,10 +1,12 @@
-import { isProperlyNested } from '../src/brackets-solution';
+import { isProperlyNested } from '../src/brackets-exercise';
 
 it.each`
   input         | output
+  ${''}         | ${1}
   ${'{[()()]}'} | ${1}
   ${'([)()]'}   | ${0}
   ${'{{{{'}     | ${0}
+  ${')'}        | ${0}
 `('is properly nested $input.length $output', ({ input, output }) => {
   const isNested = isProperlyNested(input);
 
