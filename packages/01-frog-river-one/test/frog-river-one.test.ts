ce2484e51569f69fbb91719ef25d3d3c8a9a750c
Joao Franco <joao.franco@jsfsi.com>
2024-09-25 19:59:21 +0100

    Improve tests

--- packages/01-frog-river-one/test/frog-river-one.test.ts
+++ packages/01-frog-river-one/test/frog-river-one.test.ts
@@ -3,6 +3,7 @@ import { earliestJump } from '../src/frog-river-one-exercise';
 it.each`
   jumps | leafs                       | output
   ${5}  | ${[1, 3, 1, 4, 2, 3, 5, 4]} | ${6}
+  ${5}  | ${[1, 3, 1, 4, 2, 3, 4, 4]} | ${-1}
   ${5}  | ${[3]}                      | ${-1}
   ${1}  | ${[1]}                      | ${0}
 `(
